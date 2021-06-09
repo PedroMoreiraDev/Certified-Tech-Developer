137d41aae851df879014eeefb06135da0ce50346
PedroMoreiraDev <pedrodev2020@gmail.com>
2021-06-08 23:18:24 -0300

    incluindo mais exercicios de prog imperativa

--- programacao-imperativa/04-exercicio/arrays.js
+++ programacao-imperativa/04-exercicio/arrays.js
@@ -1,7 +1,30 @@
 let filmes = ["star wars", "clube da luta",  "o poderoso chefão", "top gun",  "interestelar"];
 
-let cartoons = ["toy story", "Procurando Nemo", "kung-fu panda", "wally", "fortnite"];
+//==============Modifique os elementos do array para letra maiuscula=================
 
-cartoons.pop();
+
+function converterParaMaiusculo(item, indice)
+{
+    filmes[indice] = filmes[indice].toUpperCase();
+}
+   
+
+filmes.forEach(converterParaMaiusculo)
+
+console.log(filmes);
+
+//=====================retire o nome de "jogo" do segundo array abaixo===============
+
+let cartoons = ["toy story", "Procurando Nemo", "kung-fu panda", "wally", "fortnite"]
+
+cartoons.pop()
 
 console.log(cartoons)
+
+//==========crie função que adiciona array de cartoons no array de filmes============
+
+
+  let filmesEfilmesAnimados = [].concat(filmes,cartoons)
+
+
+console.log(filmesEfilmesAnimados)
