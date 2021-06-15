8373e32ab30060a63116d3dc2f47944f03b886e3
PedroMoreiraDev <pedrodev2020@gmail.com>
2021-06-15 19:06:32 -0300

    atualizando front end

---
+++ programacao-imperativa/09-exercicio/arrow-calcular-imc.js
@@ -0,0 +1,5 @@
+/*Criar uma arrowfunction que utilize a altura e o peso para calcular o IMC de um individuo */
+
+let calcularIMC = (altura, peso) => console.log(peso/(altura*altura))
+
+calcularIMC(1.75, 65);
