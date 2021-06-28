1e2d0a51489371ca7c4da75ee7156053ba3a3374
PedroMoreiraDev <pedrodev2020@gmail.com>
2021-06-28 19:43:04 -0300

    enviando final do poo

--- programacao-imperativa/13-avaliacao/microondasoop.js
+++ programacao-imperativa/13-avaliacao/microondasoop.js
@@ -1,45 +1,49 @@
-let microondas = function(construcTime, constructFood)
-{
 
+    //funcao de criacao
+    function UserInput(time, inputFood){
+        this.time = time
+        this.inputFood = inputFood
+    }
+
+    //funcao criação > aonde o usuário insere o input
+    const userChoice = new UserInput(15, 'pipoca')
+
+// obj ja cadastrado no sistema
 let foodOptions = {
 food: ['pipoca', 'macarrao', 'carne', 'feijão', 'brigadeiro'],
 time: [10, 8, 15, 12, 8]
 }
 
-//funcao de criacao
-    function UserInput(time, inputFood){
-    this.time = time
-    this.inputFood = inputFood
-}
-const userChoice = new UserInput(construcTime, constructFood)
-
+function microondas()
+{
 
 //loop para percorrer food0ptions e comparar com a criacao
 
+    
+
     for (i = 0; i < foodOptions.food.length; i++)
+    if(userChoice.inputFood == foodOptions.food[i])
     {
-        if(userChoice.inputFood.includes(!foodOptions.food[i]))
         {
-            return  console.log("prato inexistente")
+        if(userChoice.inputFood.includes(foodOptions.food[i]) && userChoice.time > foodOptions.time[i]*3)
+        {
+            return console.log("kabumm")
         }
-        else if(userChoice.inputFood == foodOptions.food[i] && userChoice.time > foodOptions.time[i]*2)
+        else if(userChoice.inputFood.includes(foodOptions.food[i]) && userChoice.time > foodOptions.time[i]*2)
         {
             return console.log("a comida queimou")
         }
         else
-        if(userChoice.inputFood == foodOptions.food[i] && userChoice.time < foodOptions.time[i])
+        if(userChoice.inputFood.includes( foodOptions.food[i]) && userChoice.time < foodOptions.time[i])
         {
         return console.log("tempo insuficiente")
         }
-        else 
-        if(userChoice.inputFood == foodOptions.food[i] && userChoice.time > foodOptions.time[i]*3)
-        {
-            return console.log("kabumm")
-        }
-        else
         return console.log("prato pronto, bom apetite!")
     }
+
 }
-microondas(100, 'pipoca')
+console.log("prato inexistente")
+}
+microondas()
 
 
